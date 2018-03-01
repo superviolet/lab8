02229c0277dbfdd746da04c5286061be5fc6f2f8
John Daniel Domine <jdomine@ucsd.edu>
2018-03-01 10:53:15 -0800

    Finished lab

--- public/js/googleMaps.js
+++ public/js/googleMaps.js
@@ -1,3 +1,16 @@
 function initMap() {
 	// add your code here
+	var ucsd_ltlng = {lat:32.8788033, lng:-117.23591210000001};
+
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: ucsd_ltlng,
+		zoom: 15
+	});
+
+	var marker = new google.maps.Marker({
+		position: ucsd_ltlng,
+		map: map,
+		title: "UCSD"
+	});
+
 }
